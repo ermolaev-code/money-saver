342571f326435e0ff5f8ebae75adcf3695e0a174
Yermocoding <yermocoding@gmail.com>
2022-10-04 15:53:00 +0300

    Added media queries hook

--- src/ui/components/Layout/Layout.tsx
+++ src/ui/components/Layout/Layout.tsx
@@ -1,22 +1,14 @@
 import React from 'react'
-// import { Outlet } from 'react-router-dom'
-// import { useAppRoutes } from '@/lib/hooks'
-// import Sidebar from './Sidebar'
-// import Menu from './Menu'
-import { useMql } from './useMql'
+import { Sider, Content, Footer } from '@/ui/components'
 import './index.scss'
 
 export default function Layout() {
-  // const { menuItems, active } = useAppRoutes()
-  const query = useMql()
-  console.log(query)
-
   return (
     <div className="layout">
-      <div className="sidebar">Sidebar</div>
+      <Sider />
       <div className="layout">
-        <div className="content">Content</div>
-        <div className="footer">Footer</div>
+        <Content />
+        <Footer />
       </div>
     </div>
   )
--- src/ui/components/Layout/Sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import React, { ReactNode } from 'react'
-
-type TSidebar = {
-  children: ReactNode
-}
-
-export default function Sidebar({ children }: TSidebar) {
-  return <div className="app-sidebar">{children}</div>
-}
